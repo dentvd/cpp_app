ddad954f1fdf215fa688ef60c835ccfc91d9cf7a
dentvd <deniskovalchuk79@gmail.com>
2022-10-20 00:55:57 +0300

    Плутанина

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    bool j=20.31, g=41.76, c=1.5, m=15;
-    int nj=80, ng=24, nc=54, nm=18;
+    bool j=20.31, g=41.76, c=1.5, m=15;//вартість одиниці товару
+    int nj=80, ng=24, nc=54, nm=18;//початкова наявність 
     cout << "\t\t\t#>----------< Product >-----------<#" << endl;
     cout << "\t\t\t|    1 - jewelry set  - 20.31 $    |" << endl;
     cout << "\t\t\t|    2 - garland      - 41.76 $    |" << endl;
@@ -17,24 +17,30 @@ int main()
     cin >> number;
     system("cls");
     switch (number)
-    {case 1://zj=nj-n
+    {case 1:
         int n,zj;
-        while (zj != 0||n>0)
-     { 
+       
+     
         cout << "\tEnter quantity=>";
         cin >> n;
+        while(n!=0||n<zj)
+        {
         zj= nj - n;
 
-    {if(zj>=n)
+        {if (zj >= n)
         {
-        cout << "\tjewelry set" << n << "* 20.31$\t" << n * j;
+            double sumj = n * 20.31;
+            cout << "\tjewelry set   " << n << "x 20.31$\t" << sumj << "$";
         }
-    else if (zj < n)
+        else if (zj < n)
         {
-        cout << "\TNOT AVAILABLE!!! REMAINDER => " << zj << endl;
-        cin >> n;
+            cout << "\tNOT AVAILABLE!!! REMAINDER => " << nj << endl;
+            cout << "\tEnter quantity(Exit 0)=>";
+            cin >> n;
+
         }
-     }
+        }
+     
 
     }break;
 
