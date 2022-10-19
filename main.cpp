062df982fe4c80da8c01d30f50d77ac073d0c245
dentvd <deniskovalchuk79@gmail.com>
2022-10-19 17:38:23 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
+	int min, max, a;
+	cout << "\tEnter range start=>";
+	cin >> min;
+	cout << "\tEnter end of range=>";
+	cin >> max;
+	cout << "\tEnter number=>";
+	cin >> a;
 	
+	while (a<min||a>max)
+	{
+		
+		cout << "\tEnter number=>";
+		cin >> a;
+		
+	}
+    	cout << "\tThe number is in the range";
 	return 0;
 }
