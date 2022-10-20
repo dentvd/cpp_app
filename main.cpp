c57577a8d976c88f7485e3bd70bd54d80c54ba78
dentvd <deniskovalchuk79@gmail.com>
2022-10-20 18:07:23 +0300

    Необмежена кількість виводу товара

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    double j=20.31, g=41.76, c=1.5, m=15;//вартість одиниці товару
-    int nj=80, ng=24, nc=54, nm=18;//початкова наявність 
+    double j = 20.31, g = 41.76, c = 1.5, m = 15;//вартість одиниці товару
+    int nj = 80, ng = 24, nc = 54, nm = 18;//початкова наявність 
     cout << "\t\t\t#>----------< Product >-----------<#" << endl;
     cout << "\t\t\t|    1 - jewelry set  - 20.31 $    |" << endl;
     cout << "\t\t\t|    2 - garland      - 41.76 $    |" << endl;
@@ -13,69 +13,72 @@ int main()
     cout << "\t\t\t|    Exit - 0                      |" << endl;
     cout << "\t\t\t#>--------------------------------<#" << endl;
     cout << "\t\t\tEnter product =>";
-    
-    int number;
-    cin >> number;
-    system("cls");
-
-    switch (number)
-    {
-    case 1:
-    {   int n;
+    int number=1;
     do
     {
-
-        cout << "\tEnter quantity=>";
-        cin >> n;
-        if (nj >= n)
-        {
-            double sumj = n *j;
-            cout << "\tjewelry set   " << n << "x 20.31$\t" << sumj << "$";
-
-            nj -= n;
-            break;
-        }
-        else if (nj < n)
-        {
-            cout << "\tNOT AVAILABLE!!! REMAINDER => " << nj << endl;
-
-            
-        }
-
-
-    }
-        while (n!= 0 || n <= nj);
-        //cout << "\tEnter quantity(Exit 0)=>";
-        //cin >> n;
-    }break;
-    case 2:
-    { 
-        int n;
         
+        cin >> number;
+
+        switch (number)
+        {
+        case 1:
+        {   int n;
         do
         {
 
             cout << "\tEnter quantity=>";
             cin >> n;
-            if (ng >= n)
+            if (nj >= n)
             {
-                double sumg = n * g;
-                cout << "\tgarland   " << n << "x 41.76$\t" << sumg << "$";
-                ng -= n;
+                double sumj = n * j;
+                cout << "\tjewelry set   " << n << "x 20.31$\t" << sumj << "$";
+
+                nj -= n;
                 break;
             }
-            else if (ng < n)
+            else if (nj < n)
             {
-                cout << "\tNOT AVAILABLE!!! REMAINDER => " << ng << endl;
+                cout << "\tNOT AVAILABLE!!! REMAINDER => " << nj << endl;
 
-                
+
             }
 
 
-        } while (n != 0 || n < ng);
-    }break;
+        } while (n != 0 || n <= nj);
+        //cout << "\tEnter quantity(Exit 0)=>";
+        //cin >> n;
+        }break;
+        case 2:
+        {
+            int n;
 
-    }return 0;
-	
+            do
+            {
+
+                cout << "\tEnter quantity=>";
+                cin >> n;
+                if (ng >= n)
+                {
+                    double sumg = n * g;
+                    cout << "\tgarland   " << n << "x 41.76$\t" << sumg << "$";
+                    ng -= n;
+                    break;
+                }
+                else if (ng < n)
+                {
+                    cout << "\tNOT AVAILABLE!!! REMAINDER => " << ng << endl;
+
+
+                }
+                
+
+            } while (n != 0 || n < ng);
+        }break;
+        }
+        cout << endl<<"\t\t\tEnter product =>";
+    }while (number!=0);
+    return 0;
 }
+
+	
 	
