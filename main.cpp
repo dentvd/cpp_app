f378b1cbb10039bb74aa572683276e1ff34a20c6
dentvd <deniskovalchuk79@gmail.com>
2022-10-20 15:27:28 +0300

    Заготівка

--- main.cpp
+++ main.cpp
@@ -1,22 +1,10 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 int main()
 {
-	int min, max, a;
-	cout << "\tEnter range start=>";
-	cin >> min;
-	cout << "\tEnter end of range=>";
-	cin >> max;
-	cout << "\tEnter number=>";
-	cin >> a;
-	
-	while (a<min||a>max)
-	{
-		
-		cout << "\tEnter number=>";
-		cin >> a;
-		
-	}
-    	cout << "\tThe number is in the range";
+	int a;
+	a = 1 + srand(time(0))%10;
+
 	return 0;
 }
