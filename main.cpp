b9a6b63fa91d7bb69e528e534e29d35b8b9b3dab
dentvd <deniskovalchuk79@gmail.com>
2022-10-24 16:26:14 +0300

    Рандом

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
-#include<ctime>
+#include<time.h>
+#include<chrono>
+#include<random>
+#include<stdio.h>
 using namespace std;
 int main()
 {
-	int a;
-	a = 1 + srand(time(0))%10;
-
+	random_device rd;
+	uni
+	int i = 0;
+	srand(time(0));
+	int a,b;
+	a = rand() % 10;
+	srand(time(0));
+	b = rand() % 10;
+	cout << a;
+	cout << b;
 	return 0;
 }
